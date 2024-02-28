357cf9d3af8ad6d86147109d019ec1ce1a51158b
monkey <317437084@qq.com>
2024-02-28 15:18:53 +0800

    update tabbar item

--- LocalPods/Platform/Platform/Classes/BaseUI/BaseTabBarController.swift
+++ LocalPods/Platform/Platform/Classes/BaseUI/BaseTabBarController.swift
@@ -18,8 +18,10 @@ open class BaseTabBarController: UITabBarController {
     private func setupTabBar() {
         if #available(iOS 15.0, *) {
             let appearance = UITabBarAppearance()
-            appearance.backgroundColor = .lightGray
-            appearance.backgroundEffect = nil
+            appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor : UIColor.black]
+            appearance.stackedLayoutAppearance.normal.iconColor = .black
+            appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor : UIColor.cyan]
+            appearance.stackedLayoutAppearance.selected.iconColor = .cyan
             self.tabBar.scrollEdgeAppearance = appearance
             self.tabBar.standardAppearance = appearance
         } else {
